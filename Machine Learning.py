7b4651a85d7c7d9f6ab3eed62af4528d52f86c8d
Giang Git Kraken <vangiang260694@gmail.com>
2021-10-10 18:57:05 +0700

    Practice SQL Alchemy

--- Machine Learning.py
+++ Machine Learning.py
@@ -1,15 +1,12 @@
-
-from typing import Text
-import numpy as np
-import pandas as pd
-import sqlalchemy as sal
 from sqlalchemy import create_engine
-import pyodbc
-from sqlalchemy import text
-from sqlalchemy import select
-import sqlite3
 from sqlalchemy import engine
-from sqlalchemy.engine.base import Connection
+from sqlalchemy import MetaData
+from sqlalchemy.sql.sqltypes import Integer, String
+from sqlalchemy import Table, Column, true, false
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+# class User(Base):
 
 '''
 d = {'A':[1,2,np.nan], 'B':[5,np.nan,np.nan], 'C':[1,2,3]}
@@ -72,14 +69,41 @@ print("Print Data Successfully")
 
 cursor.execute('SELECT * FROM EdgeSealInspection')
 print (cursor.fetchall())
-'''
 
-engine = create_engine("sqlite:///NguyenVanGiang.db")
+engine = create_engine("sqlite:///EdgeSealInspection.db")
+meta_data_obj = MetaData()
+user = Table('user', meta_data_obj,
+Column('user_id', Integer, primary_key=True),
+Column('user_name', String(16), nullable=false),
+Column('email_address', String(60)),
+Column('nick_name', String(50), nullable=false))
+user.create(engine)
+
+engine = create_engine('sqlite:///EdgeSealInspection.db')
+
+meta_data_obj = MetaData()
+employees = Table('employees', meta_data_obj,
+Column('employee_id', Integer, primary_key=True, key='Employee_ID'),
+Column('employee_name', String(60), nullable=false, key='Employee_Name'),
+Column('employee_dept', Integer, key='Employee_Department'))
+
+employees.create(engine, checkfirst=true)
+print(employees.columns.Employee_Name.type)
+
+# employees.create(engine, checkfirst=True)
+# employees.drop(engine, checkfirst=True)
+
+
+# bật cái checkfirst lên là true để check xem bản tồn tại hay chưa
+# để raise alarm nếu bảng có rồi mà còn tạo thêm bảng nữa
+# schema.table là lớp table, key để dùng nhận dạng cột thông qua lớp table
+
 # engine.execute('CREATE TABLE Demo (Giang TEXT, Dung TEXT, Nhu INTEGER, Duyen INTEGER)')
 # engine.execute("INSERT INTO Demo VALUES ('24', '21', '32', '12')")
-result = engine.execute('SELECT * FROM Demo')
-print(result.fetchall())
+# result = engine.execute('SELECT * FROM Demo')
+# print(result.fetchall())
 
 # git commit --amend -m "Create Data Frame With Missing Value " // Change Name Of Commit
 # threshold is require how many non na value kept
 # sqlite cũng giống như sqlite3 nhưng sqlite3 thì nâng cấp hơn
+'''
